f23321ba3500c78baedb7ecaf7b55a211d7ab6b2
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-08-03 12:52:35 +0300

    done

--- grouping/src/RegistrationForm.js
+++ grouping/src/RegistrationForm.js
@@ -1,134 +1,131 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 
 function RegistrationForm() {
-
   // State to store the form data
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        dateOfBirth: "",
-        gender: "",
-        phone: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });
-    
-      // Event handler to update the form data when input fields change
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-      };
-    
-       // Event handler to submit the form data to the server
-      const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        // Making a POST request to the API endpoint with the form data
-        fetch("http://localhost:8000/personalDetails", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("Data posted successfully:", data);
-        })
-        .catch((error) => console.error("Error posting data:", error));
-  
-      // Resetting the form fields after submission
-      setFormData({
-        firstName: "",
-        lastName: "",
-        dateOfBirth: "",
-        gender: "",
-        phone: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });
-    };
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    gender: "",
+    phone: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
+  // Event handler to update the form data when input fields change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
 
-    return (
-      <div className="Container">
-        <div className="Container-card">
-          <p>Passenger Details</p>
-        </div>
-        <div className="Progress-bar">
-        <div class="stage">
-                      <p class="tool-tip">Personal info</p>
-                      <p class="stageno stageno-1">1</p>
-                  </div>
-                  <div class="stage">
-                      <p class="tool-tip">Contact info</p>
-                      <p class="stageno stageno-2">2</p>
-                  </div>
-                  <div class="stage">
-                      <p class="tool-tip">Final Submit</p>
-                      <p class="stageno stageno-3">3</p>
-                  </div>
-        </div>
-        <div className="signup-form-container">
-        <div className="stage1-content">
-          <div className="button-container">
-            <div className="text-fields fname">
-              <label htmlFor="fname">First Name:</label>
-              <input type="text" name="fname" id="fname" placeholder="Enter your 
-              first name" />
-            </div>
-            <div className="text-fields lname">
-              <label htmlFor="lname">Last Name:</label>
-              <input type="text" name="lname" id="lname" placeholder="Enter your 
-              last name" />
-            </div>
-          </div>
-          <div className="button-container">
-            <div className="text-fields dob">
-              <label htmlFor="dob">Date of Birth:</label>
-              <input type="date" name="dob" id="dob" />
-            </div>
-            <div className="gender-selection">
-              <p className="field-heading">Gender:</p>
-              <label htmlFor="male">
-                <input type="radio" name="gender" id="male" />Male
-              </label>
-              <label htmlFor="female">
-                <input type="radio" name="gender" id="female" />Female
-              </label>
-            </div>
-          </div>
+  // Event handler to submit the form data to the server
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        
-          <div className="information">
-            <div className="header">
-            <h2>Contact details</h2>
-            <div className="writing">
-                <p>Please provide your contact details so that we can notify your updates on your flight</p>
-            </div>
-            </div>
-          </div>
-          <div className="signup-form-content">
-      <div className="stage1-content">
-        <div className="text-fields phone">
-          <label htmlFor="phone">Phone Number:</label>
-          <input type="text" name="phone" id="phone" placeholder="Phone number" />
+    // Making a POST request to the API endpoint with the form data
+    fetch("http://localhost:8000/personalDetails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data posted successfully:", data);
+      })
+      .catch((error) => console.error("Error posting data:", error));
+
+    // Resetting the form fields after submission
+    setFormData({
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      gender: "",
+      phone: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  return (
+    <div className="Container">
+
+      {/* Form for user registration */}
+      <form onSubmit={handleSubmit}>
+        {/* First Name */}
+        <div className="text-fields fname">
+          <label htmlFor="fname">First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            id="fname"
+            value={formData.firstName}
+            onChange={handleChange}
+            placeholder="Enter your first name"
+          />
         </div>
-        <div className="text-fields email">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" placeholder="Enter your email id" />
+
+        {/* Last Name */}
+        <div className="text-fields lname">
+          <label htmlFor="lname">Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            id="lname"
+            value={formData.lastName}
+            onChange={handleChange}
+            placeholder="Enter your last name"
+          />
         </div>
-      </div>
-     
+
+        {/* Date of Birth */}
+        <div className="text-fields dob">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            id="dob"
+            value={formData.dateOfBirth}
+            onChange={handleChange}
+          />
+        </div>
+
+        {/* Gender */}
+        <div className="gender-selection">
+          <p className="field-heading">Gender:</p>
+          <label htmlFor="male">
+            <input
+              type="radio"
+              name="gender"
+              id="male"
+              value="Male"
+              checked={formData.gender === "Male"}
+              onChange={handleChange}
+            />
+            Male
+          </label>
+          <label htmlFor="female">
+            <input
+              type="radio"
+              name="gender"
+              id="female"
+              value="Female"
+              checked={formData.gender === "Female"}
+              onChange={handleChange}
+            />
+            Female
+          </label>
+        </div>
+
+        {/* ... (rest of the form fields) ... */}
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
-          
-        </div>
-      </div>
-      </div>
-    );
-  }
-  
-  export default RegistrationForm;
-  
+  );
+}
+
+export default RegistrationForm;
