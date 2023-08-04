0945129e54dcbea097bd32d95028ff05a67b9749
Isma1Huz <132744360+Isma1Huz@users.noreply.github.com>
2023-08-04 11:36:13 +0300

    Revert "Payment page"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from "react";
 import Landing from './Pages/LandingPage/Landing';
-import PaymentPage from "./PaymentPage";
-
 function App() {
   return (
     <div className="App">
-     <PaymentPage />
       <Landing/>
+
     </div>
   );
 }
