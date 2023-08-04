bf3c424074b60107c6cd8b376ba1bc5ceb2b65c1
Isma1Huz <132744360+Isma1Huz@users.noreply.github.com>
2023-08-04 11:47:33 +0300

    Revert "Revert "Revert "Revert "Login page""""
    This reverts commit 514dccffb9bec0d33222dbd239ab7f783222ea16.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+
+
+import React from 'react';
 import Landing from './Pages/LandingPage/Landing';
 import PaymentPage from "./PaymentPage";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import Link here
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import ForgotPasswordForm from './components/ForgotPasswordForm';
+import Landing from './Pages/LandingPage/Landing';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+
+    <>
      <PaymentPage />
       <Landing/>
-    </div>
-  );
+    <Router>
+      <div className="app">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Login</Link> 
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link> 
+              </li>
+              <li>
+                <Link to="/forgot-password">Forgot Password</Link> 
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+        </Routes>
+      </div>
+    </Router>
+    </>)
 }
 
-export default App;
+
