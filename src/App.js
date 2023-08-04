ffb908b1e436723f40e789280570e04258752481
Isma1Huz <132744360+Isma1Huz@users.noreply.github.com>
2023-08-04 11:42:50 +0300

    Revert "Revert "Payment page""
    This reverts commit b2765ecbc2d9b9467750f4d3f74b27858af6804f.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import Landing from './Pages/LandingPage/Landing';
+import PaymentPage from "./PaymentPage";
+
 function App() {
   return (
     <div className="App">
+     <PaymentPage />
       <Landing/>
-
     </div>
   );
 }
